9867f8d0cf36b2a08470fe809f178ceea9df1aaf
Pepe Padial <jose.carlos.padial@gmail.com>
2018-02-24 18:09:48 +0100

    Replace UnbufferedKeyboardHandler with a require_relative

--- lib/websocket_client.rb
+++ lib/websocket_client.rb
@@ -1,50 +1,6 @@
 require 'termios'
 require 'websocket-eventmachine-client'
-
-ARROWS = {
-  up:    ["\e", "[", "A"],
-  down:  ["\e", "[", "B"],
-  right: ["\e", "[", "C"],
-  left:  ["\e", "[", "D"],
-}
-WASD_KEYS = %W(w a s d W A S D)
-WASD_MOVES = {
-  w: :up,
-  s: :down,
-  d: :right,
-  a: :left,
-}
-
-module UnbufferedKeyboardHandler
-  def initialize(ws)
-    @ws = ws
-    @buffer = Array.new(3)
-  end
-
-  def receive_data(data)
-    @buffer.shift
-    @buffer << data
-
-    ARROWS.keys.each do |key|
-      if @buffer == ARROWS[key]
-        @ws.send key
-      end
-    end
-
-    WASD_KEYS.each do |key|
-      if got_non_arrow_key(key)
-        @ws.send WASD_MOVES[key.downcase.to_sym]
-      end
-    end
-  end
-
-  private
-
-  def got_non_arrow_key(key)
-    @buffer.last == key && @buffer[-2] != "["
-  end
-end
-
+require_relative 'unbuffered_keyboard_handler.rb'
 
 EM.run do
 
